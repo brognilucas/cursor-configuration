3fb9a33d6b5a2af1350602b991a2b305984e12c8
Lucas Brogni <lucasbrogni16@gmail.com>
2025-06-24 10:13:52 +0200

    Refactor ShoppingCart unit tests: single behaviour per test, blank line separation

--- shopping_cart/src/__tests__/ShoppingCart.test.ts
+++ shopping_cart/src/__tests__/ShoppingCart.test.ts
@@ -17,19 +17,29 @@ describe('ShoppingCart', () => {
     expect(products).toHaveLength(0);
   });
 
-  it('lists a single product when one product is added to cart', async () => {
+  it('lists a single product when one product is added to cart (length)', async () => {
     const product = new Product('1', 'Test Product', 20);
     const cart = new ShoppingCart(repository);
 
     await cart.addProduct(product);
 
     const products = await cart.listProducts();
-    
+
     expect(products).toHaveLength(1);
+  });
+
+  it('lists a single product when one product is added to cart (equality)', async () => {
+    const product = new Product('1', 'Test Product', 20);
+    const cart = new ShoppingCart(repository);
+
+    await cart.addProduct(product);
+
+    const products = await cart.listProducts();
+
     expect(products[0]).toEqual(product);
   });
 
-  it('lists multiple products when multiple products are added to cart', async () => {
+  it('lists multiple products when multiple products are added to cart (length)', async () => {
     const product1 = new Product('1', 'First Product', 20);
     const product2 = new Product('2', 'Second Product', 30);
     const product3 = new Product('3', 'Third Product', 40);
@@ -40,10 +50,52 @@ describe('ShoppingCart', () => {
     await cart.addProduct(product3);
 
     const products = await cart.listProducts();
-    
+
     expect(products).toHaveLength(3);
+  });
+
+  it('lists multiple products when multiple products are added to cart (contains product1)', async () => {
+    const product1 = new Product('1', 'First Product', 20);
+    const product2 = new Product('2', 'Second Product', 30);
+    const product3 = new Product('3', 'Third Product', 40);
+    const cart = new ShoppingCart(repository);
+
+    await cart.addProduct(product1);
+    await cart.addProduct(product2);
+    await cart.addProduct(product3);
+
+    const products = await cart.listProducts();
+
     expect(products).toContainEqual(product1);
+  });
+
+  it('lists multiple products when multiple products are added to cart (contains product2)', async () => {
+    const product1 = new Product('1', 'First Product', 20);
+    const product2 = new Product('2', 'Second Product', 30);
+    const product3 = new Product('3', 'Third Product', 40);
+    const cart = new ShoppingCart(repository);
+
+    await cart.addProduct(product1);
+    await cart.addProduct(product2);
+    await cart.addProduct(product3);
+
+    const products = await cart.listProducts();
+
     expect(products).toContainEqual(product2);
+  });
+
+  it('lists multiple products when multiple products are added to cart (contains product3)', async () => {
+    const product1 = new Product('1', 'First Product', 20);
+    const product2 = new Product('2', 'Second Product', 30);
+    const product3 = new Product('3', 'Third Product', 40);
+    const cart = new ShoppingCart(repository);
+
+    await cart.addProduct(product1);
+    await cart.addProduct(product2);
+    await cart.addProduct(product3);
+
+    const products = await cart.listProducts();
+
     expect(products).toContainEqual(product3);
   });
 }); 
